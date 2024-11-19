845d35d750a3cb89eff5121fba61802afa0d5b9d
Yuragranovsky <yuragranovsky@gmail.com>
2024-11-19 12:43:38 +0200

    HW #41 solution

--- company-server-appl/src/main/java/telran/employees/Main.java
+++ company-server-appl/src/main/java/telran/employees/Main.java
@@ -1,17 +1,22 @@
 package telran.employees;
 
 import telran.io.Persistable;
+import telran.io.PersistableSaverThread;
 import telran.net.TcpServer;
 
 public class Main {
     private static final String FILE_NAME = "employees.data";
         private static final int PORT = 4000;
-        
-            public static void main(String[] args) {
-                Company company = new CompanyImpl();
-                if (company instanceof Persistable persistable) {
-                    persistable.restoreFromFile(FILE_NAME);
+                private static final long TIME_INTERVAL = 60000;
+                
+                    public static void main(String[] args) {
+                        Company company = new CompanyImpl();
+                        if (company instanceof Persistable persistable) {
+                            persistable.restoreFromFile(FILE_NAME);
+                            PersistableSaverThread saverThread = new PersistableSaverThread(persistable, FILE_NAME, TIME_INTERVAL);
+                            saverThread.start();
             }
+           
             TcpServer tcpServer = new TcpServer(new CompanyProtocol(company), PORT);
             tcpServer.run();
     }
---
+++ company-server-appl/src/main/java/telran/io/PersistableSaverThread.java
@@ -0,0 +1,26 @@
+package telran.io;
+
+public class PersistableSaverThread extends Thread{
+    private Persistable persistable;
+    private String fileName;
+    private long timeInterval;
+    public PersistableSaverThread(Persistable persistable, String fileName, long timeInterval) {
+        this.persistable = persistable;
+        this.fileName = fileName;
+        this.timeInterval = timeInterval;
+    }
+    @Override
+    public void run(){
+        try {
+            while(true) {
+                persistable.saveToFile(fileName);
+                sleep(timeInterval);
+            }
+            
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+
+    }
+   
+}
